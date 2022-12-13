dd0919141e9f9fd3a6275a5030178a5b4ea782fb
Nicolas Yazman <nicolasyazman@gmail.com>
2022-12-13 15:53:11 +0100

    Added the code to pull from github on git push.

--- portfolio_nicolas_yazman/views.py
+++ portfolio_nicolas_yazman/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import git
 
 @csrf_exempt
-def hello(request):
-    return HttpResponse('pong')
+def update_server(request):
+    repo = git.Repo('https://github.com/nicolasyazman/Portfolio.git')
+    origin = repo.remotes.origin
+    origin.pull()
+    return HttpResponse('Updated Server with the latest Git code')
 
